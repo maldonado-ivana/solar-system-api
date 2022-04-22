dd64aee1b38d00a62961a6a9d21c1d951e024630
Lauren Kleinert <lauren.o.kleinert@gmail.com>
2022-04-22 11:10:06 -0700

    Created Planets blueprint, registered blueprint with app object, and created blueprint endpoint get_planets

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 class Planet:
     def __init__(self, id, name, description, order_in_solar_system):
@@ -17,3 +17,16 @@ planets =  [
         Planet(7, "Uranus", "smelly", "seventh"),
         Planet(8, "Neptune", "blue", "eigth")
 ]
+planets_bp = Blueprint("planets", __name__, url_prefix = "/planets")
+
+@planets_bp.route("", methods=["GET"])
+def get_planets():
+    planet_response = []
+    for planet in planets:
+        planet_response.append({
+            "id": planet.id, 
+            "name": planet.name, 
+            "description": planet.description,
+            "order in solar system": planet.order_in_solar_system
+        })
+    return jsonify(planet_response)
